ad7066fe636c99da8b422b877691de8ef2350021
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-05-18 21:32:20 -0500

    fix

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.set("view engine", "handlebars");
 
 app.use(routes);
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
     console.log(`Now listening on port http://localhost:${PORT}`)
   );
