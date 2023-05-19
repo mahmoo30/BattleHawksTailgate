84cecc5e7f03764e841886bd64a37c8327121e3c
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-05-18 22:03:27 -0500

    fix

--- models/User.js
+++ models/User.js
@@ -38,6 +38,12 @@ User.init(
     },
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
